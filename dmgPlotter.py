ef6b0f9c81e753526656ecae0583dc8d824c58a5
eric2952 <eric2952@umn.edu>
2026-05-26 10:18:13 -0500

    pulled the damageplotter class out of the distribution file

--- dmgPlotter.py
+++ dmgPlotter.py
@@ -1,20 +1,4 @@
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import customtkinter
-import diceCalculations
-from diceCalculations import *
-import dmgPlotter
-from dmgPlotter import DamagePlotter
 
-darkblue='#274f58'
-rushred='#5c092d'
-whitewords='#cdcdcd'
-steelgrey='#353b45'
-backgroundBlack='#20292f'
-
-customtkinter.set_appearance_mode("dark")
-customtkinter.set_default_color_theme("dark-blue")
-'''
 class DamagePlotter:
     def __init__(self,root,theFont):
         self.root = root
@@ -103,10 +87,3 @@ class DamagePlotter:
         self.ax.set_xlabel('Damage')
         self.ax.set_ylabel('Likelyhood')
         self.canvas.draw()
-'''
-if __name__ == '__main__':
-    root = customtkinter.CTk()
-    root.geometry("800x550")
-    aFont = customtkinter.CTkFont('Helvetica',12)
-    app = DamagePlotter(root,aFont)
-    root.mainloop()
