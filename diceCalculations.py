374bfaf597ff1645491e103cac59002eddbf6de4
eric2952 <eric2952@umn.edu>
2026-05-26 10:04:16 -0500

    Pulling the statistical functions into dedicated file

---
+++ diceCalculations.py
@@ -0,0 +1,10 @@
+def MUn(n,s):
+    return(.5*n*(s+1))
+def sigSquared(n,s):
+    MU1 = MUn(1,s)
+    return(n*(((1/6)*(s+1)*((2*s)+1))-(MU1**2)))
+def numberDistribution(x,sigma,MU):
+    returnNumber = (1/(2*3.1415926*sigma))*(2.71828**(-.5*(((x-MU)/sigma)**2)))
+    return(returnNumber)
+def hitModifier(theDC,theSave):
+    return((theDC-theSave-1)/20)
